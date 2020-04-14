94ca4612bbb7c53612b59af7d4c93b096964ce80
Max Simpson <maxsimpson95@gmail.com>
2020-04-14 19:53:15 +0800

    initial commit, started work on index and installing packages

---
+++ Develop/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+const questions = [
+    "What is your github username?",
+    "What is your repo name?"
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data,(err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+}
+
+function init() {
+
+}
+
+init();
