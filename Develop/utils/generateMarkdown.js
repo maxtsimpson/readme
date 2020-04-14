ccceb81e468016414588fa5b3a5b56c2fb85ea12
Max Simpson <maxsimpson95@gmail.com>
2020-04-14 21:11:31 +0800

    added data properties to generateMarkdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,12 +3,20 @@ function generateMarkdown(data) {
 # ${data.title}
 ${data.description}
 ## Contents
+${data.contents}
 ## Installation
+${data.installation}
 ## Usage
+${data.usage}
 ## License
+${data.license}
 ## Contributing
+${data.contributing}
 ## Tests
+${data.tests}
 ## Questions
+${data.questions}
+
 ${data.profilePic}
 ${data.profileEmail}
 `;
