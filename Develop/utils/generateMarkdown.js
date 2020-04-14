331dc555ef5b06771903600f4076865e9a0d37f6
Max Simpson <maxsimpson95@gmail.com>
2020-04-14 20:10:41 +0800

    added layout and structure for generateMarkdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,16 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
-
+${data.description}
+## Contents
+## Installation
+## Usage
+## License
+## Contributing
+## Tests
+## Questions
+${data.profilePic}
+${data.profileEmail}
 `;
 }
 
